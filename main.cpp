f1a50a8ab24f9456da0df88d1a6c507c54c2c875
zeno1233 <zenodrdbj@gmail.com>
2025-05-13 12:30:39 +0100

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-bool checkSort(int* p_arr){
-
-    for(int i=0;i < 4;){
-        if(*p_arr > *(p_arr+1)){
+bool checkSort(int* p_arr,int size){
+    int* tp_arr = p_arr;
+    for(int i=0;i < size-1;){
+        if(*tp_arr > *(tp_arr+1)){
             return false;
             }
-            p_arr++;
+            tp_arr++;
             i++;
         }
     return true;
@@ -14,10 +14,10 @@ bool checkSort(int* p_arr){
 
 }
 
-void arrSort(int* p_arr){
+void arrSort(int* p_arr,int size){
     if(p_arr!=nullptr){
-    while(!checkSort(p_arr)){
-        for(int i=0;i < 4;){
+    while(!checkSort(p_arr,size)){
+        for(int i=0;i < size-1;){
             if(*p_arr > *(p_arr+1)){
                 int temp = *p_arr;
                 *p_arr = *(p_arr+1);
@@ -26,24 +26,34 @@ void arrSort(int* p_arr){
             p_arr++;
             i++;
         }
-        p_arr -= 4;
+        p_arr -= size-1;
     }
 }
     return;
 }
 
 
-void printArr(int* p_arr){
-    for(int i{0}; i <= 4;){
+void printArr(int* p_arr,int size){
+    std::cout << "printing the sorted array of size " << size << " \n";
+    for(int i{0}; i <= size-1;){
         std::cout << *p_arr << ' ';
         p_arr++;
         i++;
     }
 }
 
-void getArrFromUser(int arr[5]){
-    std::cout << "enter a 5 element array: \n";
-    for(int i{0};i < 5;){
+int getSizeFromUser(){
+    int size{};
+    std::cout << "enter the size of the array: \n";
+    std::cin >> size;
+
+
+    return size;
+}
+
+void getArrFromUser(int arr[],int size){
+    std::cout << "enter the elements of the array: \n";
+    for(int i{0};i < size;){
         std::cin >> arr[i];
         i++;
     }
@@ -53,10 +63,11 @@ void getArrFromUser(int arr[5]){
 
 
 int main(){
-    int arr[5] = {};
-    getArrFromUser(arr);
-    arrSort(arr);
-    printArr(arr);
+    int size{getSizeFromUser()};
+    int arr[size]={};
+    getArrFromUser(arr,size);
+    arrSort(arr,size);
+    printArr(arr,size);
     
 
     
